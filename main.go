f5d3b75fe17bf214e637c43b19d57798add4aac9
Xuanye Wong <xuanye.wan@gmail.com>
2023-04-10 20:26:34 +0800

    :sparkles: 新增优雅启动和退出

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/dig"
@@ -24,12 +30,21 @@ func setupEnvironment() *common.HostEnvironment {
 	return &common.HostEnvironment{EnvironmentName: env}
 }
 
+func setupHttpServer(router *gin.Engine, settings *config.Settings) *http.Server {
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", settings.Port),
+		Handler: router,
+	}
+
+	return server
+}
+
 func setDependency() *dig.Container {
 
 	container := dig.New()
 
 	container.Provide(setupEnvironment)
-
+	container.Provide(setupHttpServer)
 	adapter.InitContainer(container)
 	application.InitContainer(container)
 	domain.InitContainer(container)
@@ -42,15 +57,37 @@ func main() {
 
 	c := setDependency()
 
-	// 运行容器
-	if err := c.Invoke(func(router *gin.Engine, settings *config.Settings) {
+	var server *http.Server
 
-		err := router.Run(fmt.Sprintf(":%d", settings.Port))
-		if err != nil {
-			panic(err)
-		}
+	// 运行容器
+	if err := c.Invoke(func(s *http.Server) {
+		server = s
 	}); err != nil {
 		panic(err)
 	}
+
+	// 启动 HTTP Server
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	// 监听关闭信号
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("Shutdown Server ...")
+
+	// 创建一个 context.Context 对象，设置超时时间
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// 关闭 HTTP Server
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 
 }
